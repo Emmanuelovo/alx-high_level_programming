767b7b134c212ccd3ee638edf60c1e02d69d2f1d
Emmanuelovo <ovoworks684@gmail.com>
2023-04-12 09:43:56 -0700

    FEATURE: multi, sqare, add, factorial, 2ndBiggest

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num) || num < 0) {
+    return 1;
+  }
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const num = process.argv[2];
+const result = factorial(parseInt(num));
+console.log(result);
+
